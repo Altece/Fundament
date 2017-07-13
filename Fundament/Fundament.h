4f874410cb88595241044b9ef23c816394ce0eae
Steven Brunwasser <sbrunwasser@gmail.com>
2017-07-13 11:11:03 -0700

    Initial Commit

---
+++ Fundament/Fundament.h
@@ -0,0 +1,19 @@
+//
+//  Fundament.h
+//  Fundament
+//
+//  Created by Steve Brunwasser on 7/13/17.
+//  Copyright © 2017 Steve Brunwasser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fundament.
+FOUNDATION_EXPORT double FundamentVersionNumber;
+
+//! Project version string for Fundament.
+FOUNDATION_EXPORT const unsigned char FundamentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fundament/PublicHeader.h>
+
+
